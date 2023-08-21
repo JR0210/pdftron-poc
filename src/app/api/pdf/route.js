ce354f615fb0db745b953e6493e0f0c45289ea59
Jacob Robinson <robinson.jacob.e@gmail.com>
2023-08-21 11:35:22 +0100

    added GET route test

---
+++ src/app/api/pdf/route.js
@@ -0,0 +1,40 @@
+const { PDFNet } = require("@pdftron/pdfnet-node");
+
+async function savePDFDocToBuffer(pdfDoc, PDFNet) {
+  const pdfBuffer = await pdfDoc.saveMemoryBuffer(
+    PDFNet.SDFDoc.SaveOptions.e_linearized
+  );
+  return pdfBuffer;
+}
+
+export async function GET() {
+  await PDFNet.initialize(process.env.APRYSE_KEY);
+  const main = async () => {
+    const doc = await PDFNet.PDFDoc.create();
+    const page = await doc.pageCreate();
+    doc.pagePushBack(page);
+    doc.save("blank.pdf", PDFNet.SDFDoc.SaveOptions.e_linearized);
+    const pdfBuffer = await savePDFDocToBuffer(doc);
+
+    const pdfBase64 = pdfBuffer.toString("base64");
+    const dataUri = `data:application/pdf;base64,${pdfBase64}`;
+
+    return new Response(dataUri, {
+      status: 200,
+      headers: {
+        "Content-Disposition": 'attachment; filename="blank.pdf"',
+      },
+    });
+  };
+
+  try {
+    await main();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Terminate PDFNet after all requests have been handled
+// process.on("exit", () => {
+//   PDFNet.terminate();
+// });
